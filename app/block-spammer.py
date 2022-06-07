4fb48fb9d7e94a1b2099aab884a49347b0ac250a
Mario Vazquez <mavazque@redhat.com>
2022-06-07 15:33:18 +0200

    Added max-workers parameter

--- app/block-spammer.py
+++ app/block-spammer.py
@@ -7,6 +7,7 @@ from lib.utils import *
 
 parser = argparse.ArgumentParser(description='Spam files to folder')
 parser.add_argument('-f', '--files', type=int, help='The number of files that will be created in the folder.', required=True)
+parser.add_argument('-m', '--max-workers', type=int, help='The number of concurrent threads that will run. If not set, concurrent threads will equal number of files', required=False)
 parser.add_argument('-w', '--wait-time', type=int, help='Number of seconds to wait between executions.', required=True)
 parser.add_argument('-s', '--file-size', type=int, help='Size (in MiB) for the generated files.', required=True)
 parser.add_argument('-p', '--file-path', type=str, help='Path where the files will be created.', required=True)
@@ -18,10 +19,14 @@ logger = logging.new_logger()
 
 number_of_files = args.files
 wait_time = args.wait_time
-file_size=str(args.file_size) + 'M'
-file_path=args.file_path
+file_size = str(args.file_size) + 'M'
+file_path = args.file_path
+num_workers = args.max_workers
 
-logger.info("Spammer config. Number of files: %s, Wait time: %s, File size: %s, File path: %s", number_of_files, wait_time, file_size, file_path)
+if num_workers is None or num_workers > number_of_files:
+    num_workers = number_of_files
+
+logger.info("Spammer config. Number of files: %s, Workers: %s, Wait time: %s, File size: %s, File path: %s", number_of_files, num_workers, wait_time, file_size, file_path)
 
 file_names = []
 for file_number in range(number_of_files):
@@ -29,7 +34,7 @@ for file_number in range(number_of_files):
     file_names.append(file_name)
 
 while(True):
-    with ThreadPoolExecutor(max_workers=number_of_files) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         for file_name in file_names:
             executor.submit(gen_file_on_disk, file_name, file_size, file_path, logger)
     logger.info("Sleeping for %s seconds...", wait_time)
--- app/s3-spammer.py
+++ app/s3-spammer.py
@@ -7,6 +7,7 @@ from lib.utils import *
 
 parser = argparse.ArgumentParser(description='Spam an S3 bucket.')
 parser.add_argument('-f', '--files', type=int, help='The number of files that will be created in the bucket.', required=True)
+parser.add_argument('-m', '--max-workers', type=int, help='The number of concurrent threads that will run. If not set, concurrent threads will equal number of files', required=False)
 parser.add_argument('-w', '--wait-time', type=int, help='Number of seconds to wait between executions.', required=True)
 parser.add_argument('-s', '--file-size', type=int, help='Size (in MiB) for the generated files.', required=True)
 parser.add_argument('-b', '--bucket-name', type=str, help='Name of the bucket where files will be pushed.', required=True)
@@ -19,12 +20,15 @@ logger = logging.new_logger()
 
 number_of_files = args.files
 wait_time = args.wait_time
-file_size=str(args.file_size) + 'M'
-bucket_name=args.bucket_name
-bucket_path=args.bucket_path
+file_size = str(args.file_size) + 'M'
+bucket_name = args.bucket_name
+bucket_path = args.bucket_path
+num_workers = args.max_workers
 
+if num_workers is None or num_workers > number_of_files:
+    num_workers = number_of_files
 
-logger.info("Spammer config. Number of files: %s, Wait time: %s, File size: %s, Bucket name: %s, Bucket path: %s", number_of_files, wait_time, file_size, bucket_name, bucket_path)
+logger.info("Spammer config. Number of files: %s, Workers: %s, Wait time: %s, File size: %s, Bucket name: %s, Bucket path: %s", number_of_files, num_workers, wait_time, file_size, bucket_name, bucket_path)
 
 s3_client = S3()
 
@@ -34,7 +38,7 @@ for file_number in range(number_of_files):
     file_names.append(file_name)
 
 while(True):
-    with ThreadPoolExecutor(max_workers=number_of_files) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         for file_name in file_names:
             executor.submit(gen_file_on_s3, file_name, file_size, s3_client, bucket_name, bucket_path, logger)
     logger.info("Sleeping for %s seconds...", wait_time)
